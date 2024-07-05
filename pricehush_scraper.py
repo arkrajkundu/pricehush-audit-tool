0be103ae8181636e77ae19b781158dd7f8c28588
Arkraj Kundu <83090891+arkrajkundu@users.noreply.github.com>
2024-07-06 00:54:55 +0530

    Update pricehush_scraper.py
    Now scrapes amazon prices, and out of stock status

--- pricehush_scraper.py
+++ pricehush_scraper.py
@@ -1,11 +1,12 @@
 import requests
 from bs4 import BeautifulSoup
 import pandas as pd
+headers = {'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:80.0) Gecko/20100101 Firefox/80.0'}
 
-data = {'pricehush': [], 'price': [], 'amazon': [], 'flipkart': []}
+data = {'pricehush': [], 'price': [], 'amazon': [], 'flipkart': [], 'amazon_prices': [], 'amazon_outofstock': []}
 
-base_url = "https://pricehush.com/brand/lenovo/page/"
-total_pages = 10
+base_url = "https://pricehush.com/brand/acer/page/"
+total_pages = 5
 
 urls = []
 
@@ -18,6 +19,10 @@ sublinks = []
 pricehush_prices = []
 amazon_links = []
 flipkart_links = []
+amazon_prices = []
+flipkart_prices = []
+amazon_outofstock = []
+flipkart_outofstock = []
 
 print("Requesting URLs and filling sublinks, prices...")
 
@@ -66,6 +71,34 @@ for sublink in sublinks:
       
   i += 1
 
+for amazon_link in amazon_links:
+  
+  # NULL Check
+  if(amazon_link == 'NULL'):
+    amazon_prices.append('NA')
+    amazon_outofstock.append('NA')
+    continue
+  
+  r = requests.get(amazon_link, headers=headers)
+  soup = BeautifulSoup(r.text, 'html.parser')
+  
+  # Cases
+  
+  prices = soup.select('span.priceToPay > span > span.a-price-whole') # Best case
+  if(len(prices)!=0):
+    amazon_prices.append(prices[0].get_text())
+    amazon_outofstock.append('No')
+  else:
+    prices = soup.find_all('span', class_=lambda x: x and 'a-size-medium' in x and 'a-color-success' in x) # Unavailable case
+    if(len(prices)!=0):
+      amazon_prices.append(prices[0].get_text())
+      amazon_outofstock.append('Yes')
+    else:
+      prices = soup.select('td.a-span12 > span.apexPriceToPay > span.a-offscreen') # Weird case
+      amazon_prices.append('NA')
+      amazon_outofstock.append('No')
+
+# Adding lists to data object
 
 for link in sublinks:
   data['pricehush'].append(link)
@@ -79,5 +112,13 @@ for amazon in amazon_links:
 for flipkart in flipkart_links:
   data['flipkart'].append(flipkart)
 
+for amazon_price in amazon_prices:
+  data['amazon_prices'].append(amazon_price)
+  
+for amazon_oos in amazon_outofstock:
+  data['amazon_outofstock'].append(amazon_oos)  
+
+# Exporting to excel
+
 df = pd.DataFrame.from_dict(data)
-df.to_excel("data.xlsx", index=False)
+df.to_excel("acer.xlsx", index=False)
